4ddc4112b9af5e95271f00ea9c2cfed146008aff
Mihir <mihir5malaviya@gmail.com>
2026-04-23 18:32:05 +0530

    make hello unteractive

--- practice/hello.js
+++ practice/hello.js
@@ -1,5 +1,9 @@
-const name = "Mihir";
-const now = new Date();
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const quotes = [
   "Keep going. Small steps count.",
@@ -12,6 +16,12 @@ const quotes = [
 const randomIndex = Math.floor(Math.random() * quotes.length);
 const randomQuote = quotes[randomIndex];
 
-console.log("Hello " + name);
-console.log("Current time is: " + now.toLocaleTimeString());
-console.log("Quote: " + randomQuote);
+rl.question("What is your name? ", function(name) {
+  const now = new Date();
+
+  console.log("\nHello " + name);
+  console.log("Current time is: " + now.toLocaleTimeString());
+  console.log("Quote: " + randomQuote);
+
+  rl.close();
+});
